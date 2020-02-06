f2cecf19c77f3410bc3eae7c5cc1b9f58e5c0484
Laura Jiménez Fernández <ljimen07@ucm.es>
2020-02-06 12:59:52 +0100

    creados los script del puzle. Falta añadir cosas y probar

---
+++ The curse/Assets/GameAssets/Scripts/PlayerInteractions/Puzzle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    [SerializeField]
+    GameObject targetObj;
+    [SerializeField]
+    Transform targetDestPos;
+    [SerializeField]
+    private int neededObjects;
+
+    private int activeObj = 0;
+    private bool activated = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!activated && activeObj == neededObjects)
+        {
+            activated = true;
+            StartCoroutine(Move());
+        }
+    }
+
+    public int GetActiveObj() { return activeObj; }
+
+    public void SetActiveObj(int num)
+    {
+        activeObj = num;
+    }
+
+    IEnumerator Move()
+    {
+        while(targetObj.transform.position != targetDestPos.position)
+        {
+            yield return new WaitForSeconds(0.3f);
+            //Se mueve
+        }
+    }
+}
---
+++ The curse/Assets/GameAssets/Scripts/PlayerInteractions/PuzzleObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleObject : MonoBehaviour
+{
+    Puzzle puzzle;
+
+    private void Start()
+    {
+        puzzle = GetComponentInParent<Puzzle>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pickable Object"))
+        {
+            puzzle.SetActiveObj(puzzle.GetActiveObj() + 1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pickable Object"))
+        {
+            puzzle.SetActiveObj(puzzle.GetActiveObj() - 1);
+        }
+    }
+}
