a705c3497562f6c2d8c760ab87a59d16254e5912
Laura Jiménez Fernández <ljimen07@ucm.es>
2020-04-09 20:35:36 +0200

    arreglado el bug del arma

--- The curse/Assets/GameAssets/Scripts/Weapon Stuff/SimpleShoot.cs
+++ The curse/Assets/GameAssets/Scripts/Weapon Stuff/SimpleShoot.cs
@@ -60,52 +60,62 @@ public class SimpleShoot : MonoBehaviour, IWeapon
     {
         if (canShoot)
         {
+            print("Dispara");
+            //GetComponent<Animator>().SetTrigger("Fire");
             canShoot = false;
-            GetComponent<Animator>().SetTrigger("Fire");
+            Shoot();
             StartCoroutine(Reload());
+            CasingRelease();
         }
     }
 
     public void Shoot()
     {
-       aSource.volume = 0.25f;
-       aSource.PlayOneShot(shotAudioClips[0]);
-       GameObject tempFlash;
-       GameObject bullet = Instantiate(bulletPrefab, posDisp.position, posDisp.rotation);
-       Vector3 dire = new Vector3();
-        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, lm))
+        if (!canShoot)
         {
-            //Dibuja la línea en el punto de impacto
-            dire = hit.point - posDisp.transform.position;
-            if (hit.collider.CompareTag("Enemy"))
+            print("Dispara");
+            aSource.volume = 0.25f;
+            aSource.PlayOneShot(shotAudioClips[0]);
+            GameObject tempFlash;
+            GameObject bullet = Instantiate(bulletPrefab, posDisp.position, posDisp.rotation);
+            Vector3 dire = new Vector3();
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, lm))
             {
-                //Partículas de sangre
-                CreateParticleAtPoint(enemyHitParticle, hit.point, false, Quaternion.LookRotation(hit.normal));
+                //Dibuja la línea en el punto de impacto
+                dire = hit.point - posDisp.transform.position;
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    //Partículas de sangre
+                    CreateParticleAtPoint(enemyHitParticle, hit.point, false, Quaternion.LookRotation(hit.normal));
+                }
             }
+            else
+            {
+                dire = Camera.main.transform.forward * 1000;
+            }
+            bullet.transform.rotation = Quaternion.LookRotation(dire.normalized);
+            bullet.GetComponent<Rigidbody>().AddForce(dire.normalized * shotPower, ForceMode.Impulse);
+            Destroy(bullet, 10);
+            tempFlash = Instantiate(muzzleFlashPrefab, posDisp.position, posDisp.rotation);
+            Destroy(tempFlash, 0.5f);
         }
-        else
-        {
-            dire = Camera.main.transform.forward  * 1000;
-        }
-        bullet.transform.rotation = Quaternion.LookRotation(dire.normalized);
-        bullet.GetComponent<Rigidbody>().AddForce(dire.normalized * shotPower, ForceMode.Impulse);
-        Destroy(bullet, 10);
-        tempFlash = Instantiate(muzzleFlashPrefab, posDisp.position, posDisp.rotation);
-        Destroy(tempFlash, 0.5f);
     }
 
     void CasingRelease()
     {
-        GameObject casing;
-        casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
-        casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
-        casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
-        aSource.volume = 0.4f;
-        aSource.clip = shotAudioClips[1];
-        aSource.PlayDelayed(0.65f);
-        Destroy(casing, 5);
+        if (!canShoot)
+        {
+            GameObject casing;
+            casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
+            casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
+            casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
+            aSource.volume = 0.4f;
+            aSource.clip = shotAudioClips[1];
+            aSource.PlayDelayed(0.65f);
+            Destroy(casing, 5);
+        }
     }
 
     void CreateParticleAtPoint(GameObject obj, Vector3 point, bool tint, Quaternion rotate)
