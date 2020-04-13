b555e3d04b624374ca3b3bdc06a7b4256700c4b1
Laura Jiménez Fernández <ljimen07@ucm.es>
2020-04-13 20:13:59 +0200

    Editado el world canvas controller

--- The curse/Assets/GameAssets/Scripts/UI/WorldSpaceCanvasController.cs
+++ The curse/Assets/GameAssets/Scripts/UI/WorldSpaceCanvasController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class WorldSpaceCanvasController : MonoBehaviour
 {
-    Transform player;
+    [SerializeField]
+    float offset = 1.5f;
+    Transform playerTransform;
 
     // Start is called before the first frame update
     public virtual void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        playerTransform.position = new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x, GameObject.FindGameObjectWithTag("Player").transform.position.y + offset, GameObject.FindGameObjectWithTag("Player").transform.position.z);
     }
 
 
     public virtual void Update()
     {
-        this.gameObject.transform.LookAt(player);
+        this.gameObject.transform.LookAt(playerTransform);
     }
 }
